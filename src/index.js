ac9ea38aa4d74dbed6665f0f93ec7dc40547c918
Tran Thi Thu Hong <thuhong@Trans-MacBook-Air.local>
2022-03-10 06:40:41 +0700

    add some file

--- src/index.js
+++ src/index.js
@@ -25,6 +25,11 @@ app.get('/news', (req, res) => {
   res.render('news')
 })
 
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  res.render('search')
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at https://localhost:${port}`)
 })
