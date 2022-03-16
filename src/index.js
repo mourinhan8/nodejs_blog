08c38e4cf700dd5432774b1fad17868db76a82f3
Tran Thi Thu Hong <thuhong@Trans-MacBook-Air.local>
2022-03-16 15:23:42 +0700

    add router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const path = require('path')
-const { render } = require('express/lib/response')
+const render = require('express/lib/response')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -15,20 +20,10 @@ app.use(morgan('combined'));
 app.engine('hbs', handlebars.engine({extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '/resources/views'))
-console.log("day la thu muc" ,path.join(__dirname, '/resources/views'))
+// console.log("day la thu muc" ,path.join(__dirname, '/resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  console.log(req.query)
-  res.render('search')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at https://localhost:${port}`)
