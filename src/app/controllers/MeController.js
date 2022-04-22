264601f8214bcb5c01a9a01f117de7c29d8eca36
Tran Thi Thu Hong <ngocnhannguyen@MacBook-Air-cua-Tran.local>
2022-04-22 22:45:42 +0700

    add select all

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,27 +2,27 @@ const Post = require("../models/Post");
 const { multipleMongooseToObj } = require("../../util/mongoose");
 
 class MeController {
-    // [GET] /me/stored/posts
-    storedPosts(req, res, next) {
-        Promise.all([Post.find({}), Post.countDocumentsDeleted()]).then(
-            ([posts, deletedCount]) =>
-                res.render("me/stored-posts", {
-                    deletedCount,
-                    posts: multipleMongooseToObj(posts),
-                })
-        );
-    }
+  // [GET] /me/stored/posts
+  storedPosts(req, res, next) {
+    Promise.all([Post.find({}), Post.countDocumentsDeleted()]).then(
+      ([posts, deletedCount]) =>
+        res.render("me/stored-posts", {
+          deletedCount,
+          posts: multipleMongooseToObj(posts),
+        })
+    );
+  }
 
-    // [GET] /me/trash/posts
-    trashPosts(req, res, next) {
-        Post.findDeleted({})
-            .then((posts) =>
-                res.render("me/trash-posts", {
-                    posts: multipleMongooseToObj(posts),
-                })
-            )
-            .catch(next);
-    }
+  // [GET] /me/trash/posts
+  trashPosts(req, res, next) {
+    Post.findDeleted({})
+      .then((posts) =>
+        res.render("me/trash-posts", {
+          posts: multipleMongooseToObj(posts),
+        })
+      )
+      .catch(next);
+  }
 }
 
 module.exports = new MeController();
